3fab4c9e6f535841b1652a33b44ba73ec3e17fa6
Adrian Rączka <adrian.raczka@outlook.com>
2023-05-05 19:29:23 +0200

    added steady mode

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,41 @@
 #include <cmath>
 
 extern "C" {
+
+    uint32_t pwm{0};
+    bool condition{true};
+        
+    void _pulse (ledc_timer_config_t *timer_1, ledc_channel_config_t *channel_1);
+    void _steady (uint8_t *pwm, ledc_timer_config_t *timer_1, ledc_channel_config_t *channel_1);
+
+    void _pulse (ledc_timer_config_t *timer_1, ledc_channel_config_t *channel_1) {
+        if (condition) {
+            ledc_set_duty((*channel_1).speed_mode, (*channel_1).channel, pwm++);
+            ledc_update_duty((*channel_1).speed_mode, (*channel_1).channel);
+            ets_delay_us(500);  //0.5 [ms]
+            if (pwm >= (pow(2, (*timer_1).duty_resolution) - 1) * 0.5) condition = !condition;
+        }
+        else {
+            ledc_set_duty((*channel_1).speed_mode, (*channel_1).channel, pwm--);
+            ledc_update_duty((*channel_1).speed_mode, (*channel_1).channel);
+            ets_delay_us(500);  //0.5 [ms]
+            if (pwm <= 0) condition = !condition;
+        }
+    }
+
+    void _steady (uint8_t *pwm, ledc_timer_config_t *timer_1, ledc_channel_config_t *channel_1) {
+        if ((*pwm) <= 0) {
+            (*pwm) = 0;
+        }
+        else if ((*pwm) >= 100) {
+            (*pwm) = 100;
+        }
+        //
+        ledc_set_duty((*channel_1).speed_mode, (*channel_1).channel, static_cast<uint32_t>((pow(2, (*timer_1).duty_resolution) - 1) * ((*pwm) / 100.0)));
+        ledc_update_duty((*channel_1).speed_mode, (*channel_1).channel);
+    }
+
+
     void app_main () {
         //timer conf
         ledc_timer_config_t timer_1{};
@@ -25,20 +60,24 @@ extern "C" {
         channel_1.duty = 0;
         ledc_channel_config(&channel_1);
         //
-        size_t pwm{};
-        bool condition{true}; 
-        while(true) {
-            if (condition) {
-                ledc_set_duty(channel_1.speed_mode, channel_1.channel, pwm++);
-                ledc_update_duty(channel_1.speed_mode, channel_1.channel);
-                ets_delay_us(500);  //0.5 [ms]
-                if (pwm >= pow(2, timer_1.duty_resolution) - 1) condition = !condition;
-            }
-            else {
-                ledc_set_duty(channel_1.speed_mode, channel_1.channel, pwm--);
-                ledc_update_duty(channel_1.speed_mode, channel_1.channel);
-                ets_delay_us(500);  //0.5 [ms]
-                if (pwm <= 0) condition = !condition;
+        enum Mode {pulse, steady};
+        Mode mode = pulse;
+        //
+        uint8_t pwm{10};
+        //loop
+        while (1) {
+            switch(mode) {
+                //pulse
+                case Mode::pulse:
+                _pulse(&timer_1, &channel_1);
+                break;
+                //steady
+                case Mode::steady:
+                _steady(&pwm, &timer_1, &channel_1);
+                break;
+                //default
+                default:
+                break;
             }
         }
     }
